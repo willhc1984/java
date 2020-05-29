c2d156293b10d5fd18c4844673d64b93824b5b93
unknown <willhc1984@gmail.com>
2020-05-28 23:31:31 -0300

    Projeto maven com springboot

---
+++ src/main/java/com/estudos/exemplomaven/ExemploComMavenESpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.exemplomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploComMavenESpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploComMavenESpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/exemplomaven/ExemploComMavenESpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploComMavenESpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
